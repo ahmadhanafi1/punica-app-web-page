97c2726f9bc4f8403eab68d5aaa8a75774a5f021
ahmadhanafi1 <ahmad.hanafi@gunsel.com.tr>
2024-01-04 18:26:22 +0200

    css fix, favicon and adding dashboard

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 	return (
 		<div className='w-full h-[5rem] flex items-center justify-between px-6'>
 			<img alt='logo' src={process.env.PUBLIC_URL + "/images/punica-logo.png"}
-				className='h-[3rem]' /> 
+				className='h-[2.2rem] ml-[4%]' /> 
 			<div className='flex items-center justify-end gap-[2rem] lg:gap-[5rem] w-[80%] lg:pr-10 '>
 				<div className='flex gap-4 lg:gap-12 flex-wrap'>
 					<Link className='text-[#474747] hover:text-[#999999]  w-fit lg:text-[1.2rem] transition-all duration-300 truncate'>Home</Link>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Primary } from '../../components/Button'
+import landing1 from "../../public/svg/landing1.svg"
 
 const Home = () => {
 	return (
-		<div>Home</div>
+		
+		<div className='w-full lg:mt-20'>
+			{/********************************** LANDING **********************************/}
+			<div className='w-full flex md:block flex-col relative items-center'>
+				<img alt='' src={landing1} className='md:absolute right-0  md:translate-x-[20%] w-[500px] lg:w-[700px] xl:w-[1000px]' />
+				<div className='text-center md:text-left md:w-1/3 md:mt-[4rem] lg:mt-[9rem] md:pl-[5%] 2xl:pl-[15%] pt-[2rem] flex flex-col gap-16 '>
+					<h5 className='w-[500px] leading-[1.15] text-[4vw] xl:text-6xl font-semibold pb-1'>
+						Manage Your Workforce with Fruitful Harmony
+					</h5>
+					<p className='text-[#474747] w-[500px] text-[1.1rem]'>
+						The revolutionary solution designed to bring a harmonious and seamless experience to your HR processes.
+					</p>
+					<span className='w-fit mx-auto md:mx-0'>
+						<Primary>Request a Demo</Primary>
+					</span>
+				</div>
+			</div>
+			{/********************************** Our Products **********************************/}
+			<div id='our-products' className='w-full'>
+				
+			</div>
+			
+		</div>
 	)
 }
 
